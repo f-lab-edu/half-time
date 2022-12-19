bbd04a0b967b8f0132b36956452cac8ad8e5b8e3
inmoG <inmoy@naver.com>
2022-12-20 02:59:57 +0900

    half-time for Spring Boot Project

---
+++ src/main/java/flab/halftime/HalfTimeApplication.java
@@ -0,0 +1,13 @@
+package flab.halftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalfTimeApplication.class, args);
+	}
+
+}
