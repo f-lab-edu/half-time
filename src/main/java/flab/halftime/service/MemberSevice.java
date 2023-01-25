4b8d6bcd3ca0e90c8939710346b5b7aab7aba10b
inmoG <inmoy@naver.com>
2023-01-25 17:46:56 +0900

    Create memberService

--- src/main/java/flab/halftime/service/MemberSevice.java
+++ src/main/java/flab/halftime/service/MemberSevice.java
@@ -1,15 +1,45 @@
 package flab.halftime.service;
 
+import flab.halftime.domain.MemberDTO;
 import flab.halftime.repository.MemberMapperRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberSerivce {
-    private final MemberMapperRepository memberMapperRepository;
+import java.util.List;
+import java.util.Optional;
 
-    @Autowired
-    public MemberSerivce(MemberMapperRepository memberMapperRepository) {
-        this.memberMapperRepository = memberMapperRepository;
+@Service
+public class MemberSevice {
+//    private final MemberMapperRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    private final MemberMapperRepository MemberMapperRepository;
+
+    public MemberSevice(MemberMapperRepository MemberMapperRepository) {
+        this.MemberMapperRepository = MemberMapperRepository;
     }
+
+
+    /*
+    회원가입
+     */
+    public Long join(MemberDTO memberDTO) {
+        validateDuplicateMemberDTO(memberDTO); // 중복 회원 검증
+        MemberMapperRepository.save(memberDTO);
+        return memberDTO.getId();
+    }
+
+    private void validateDuplicateMemberDTO(MemberDTO memberDTO) {
+        MemberMapperRepository.findByEmail(memberDTO.getMemberEmail())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<MemberDTO> findMemberDTOs() {
+        return MemberMapperRepository.findAll();
+    }
+
+    public Optional<MemberDTO> findOne(Long memberDTOId) {
+        return MemberMapperRepository.findById(memberDTOId);
+    }
+
 }
