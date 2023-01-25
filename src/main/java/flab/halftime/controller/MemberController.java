2491558723cffcb4ae6a23c823c9aa29f040fc64
inmoG <inmoy@naver.com>
2023-01-25 17:46:06 +0900

    Create memberController

--- src/main/java/flab/halftime/controller/MemberController.java
+++ src/main/java/flab/halftime/controller/MemberController.java
@@ -1,4 +1,33 @@
 package flab.halftime.controller;
 
-public class HomeController {
+import flab.halftime.domain.MemberDTO;
+import flab.halftime.service.MemberSevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberSevice memberSevice;
+
+    //    @GetMapping("member/save")
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "signup";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO) {
+        return "home";
+    }
+
+    @Autowired
+    public MemberController(MemberSevice memberSevice) {
+        this.memberSevice = memberSevice;
+    }
 }
