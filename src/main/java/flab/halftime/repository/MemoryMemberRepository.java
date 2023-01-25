549b20ae89300be6d148a8642f569de7b3c877c4
inmoG <inmoy@naver.com>
2023-01-25 17:43:46 +0900

    create repository file

--- src/main/java/flab/halftime/repository/MemoryMemberRepository.java
+++ src/main/java/flab/halftime/repository/MemoryMemberRepository.java
@@ -1,2 +1,42 @@
-package flab.halftime.repository;public class MemoryMemberRepository {
+package flab.halftime.repository;
+
+import flab.halftime.domain.MemberDTO;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberMapperRepository{
+
+    private static Map<Long, MemberDTO> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public MemberDTO save(MemberDTO memberDTO) {
+        memberDTO.setId(++sequence);
+        store.put(memberDTO.getId(), memberDTO);
+        return memberDTO;
+    }
+
+    @Override
+    public Optional<MemberDTO> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<MemberDTO> findByEmail(String email) {
+        return store.values().stream().filter(memberDTO -> memberDTO.getMemberEmail().equals(email)).findAny();
+    }
+
+    @Override
+    public Optional<MemberDTO> findByName(String name) {
+        return store.values().stream().filter(memberDTO -> memberDTO.getMemberName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<MemberDTO> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
